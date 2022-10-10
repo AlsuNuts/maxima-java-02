9f9f31a3f6de663e166cd42206ed86f385bc5bbd
AlsuNuts <forever-nuts@yandex.ru>
2022-10-10 20:43:04 +0300

    Init commit

--- src/main/java/org/example/Logistics.java
+++ src/main/java/org/example/Logistics.java
@@ -36,10 +36,10 @@ public class Logistics {
 
         for (Transport vehicle : vehicles) { //(int i = 0; i < vehicles().length(); i++)
             if (isShippingAvailable(city, vehicle, weight, hours)) {
-                if (profitTransport == null){
+                if (profitTransport == null ){
                     profitTransport = vehicle;
                 }
-                else if (profitTransport.getCostOfKm() > vehicle.getCostOfKm() ) {
+                else if (profitTransport.getCostOfKm() > vehicle.getCostOfKm() ) { //хотела избежать else if конструкции, но мне так идея подсказала
                     profitTransport = vehicle;
                 }
             }
@@ -47,9 +47,31 @@ public class Logistics {
         return profitTransport;
     }
     public boolean isShippingAvailable(City city, Transport transport, int weight, int hours) {
-        return !transport.isRepairing() && (transport.getCapacity() >= weight) && (city.getDistanceKm() / transport.getSpeed() <= hours) && transport.getCostOfKm() * city.getDistanceKm() != 0;
+        return !transport.isRepairing() & (transport.getCapacity() >= weight) & (city.getDistanceKm() / transport.getSpeed() <= hours) & transport.getCostOfKm() * city.getDistanceKm() != 0;
     }
 
 }
+
+/*
+            for (int i = 0; i < vehicles().length(); i++) {
+
+            if (weight > transport.getCapacity()) { //если груз больше грузоподъемности
+                break;
+            }
+            if (hours > (city.getDistanceKm() / getSpeed())) { //если не укладывается по времени
+                break;
+            }
+            if (!isRepairing()) { //если машина в ремонте
+                break;
+            }
+            if (getPrice(city) < profit | getPrice(city) != 0) { //если стоимость выгодная
+                profit = vehicle;
+
+            }
+        }
+
+
+        return profit;
+ */
 
 

69cf001f10ebf9c86b5bb999dc05e09724e071da
AlsuNuts <forever-nuts@yandex.ru>
2022-10-13 15:27:14 +0300

    Init commit

--- src/main/java/org/example/TransportFactory.java
+++ src/main/java/org/example/TransportFactory.java
@@ -1,41 +1,27 @@
 package org.example;
-/*
-Описать класс TransportFactory с методом Transport getTransport(City city, int weight, int hours) который будет в зависимости от города,
-веса и срока доставки конструировать нам транспортное средство по следующим правилам:
-
-Название и стоимость — для самолета, корабля и грузовика различные константы, выберите из сами, задайте их в начале метода.
-
-Если скорость, необходимая для выполнения заказа до 40 км/ч и город на воде, то корабль. Если нужна скорость выше 120 км/ч, и есть аэропорт, то самолет.
-В остальных случаях — грузовик.
-
-Грузоподъемность должна равняться весу груза, округленного вверх до числа кратного 500.
-
-Скорость должна равняться скорости, необходимой для прибытия в срок, округленной вверх до ближайшего десятка.
-
-Если вес кратен 500 и или скорость кратна 10, то округлять не нужно.
- */
 
 public class TransportFactory {
     public Transport getTransport(City city, int weight, int hours){
-        String planeName = "самолет";
-        float planeCostOfKm = 58.9f;
-        String shipName = "баржа";
-        float shipCostOfKm = 12.7f;
-        String truckName = "БЗНК"; //тем, кто любит гаражный панк-рок, рекомендую к прослушиванию Truckdrivers - Безумцы землю ногами крутят
-        float truckCostOfKm = 13.4f;
+        final String PLANE_NAME = "самолет";
+        final float PLANE_COST_OF_KM = 58.9f;
+        final String SHIP_NAME = "баржа";
+        final float SHIP_COST_OF_KM = 12.7f;
+        final String TRUCK_NAME = "БЗНК"; //тем, кто любит гаражный панк-рок, рекомендую к прослушиванию Truckdrivers - Безумцы землю ногами крутят
+        final float TRUCK_COST_OF_KM= 13.4f;
 
 
-        int transportSpeed = city.getDistanceKm()/hours; //скорость, необходимая лдля прибытия в срок
+
+        int roundedSpeed = city.getDistanceKm()/hours; //скорость, необходимая лдля прибытия в срок
         int transportWeight = (weight % 500 != 0) ? (int) Math.ceil(weight/500) * 500 : weight;
-        transportSpeed = (transportSpeed % 10 != 0) ? (int) Math.ceil(transportSpeed) * 10 : transportSpeed;
+        int transportSpeed = (roundedSpeed % 10 != 0) ? (int) Math.ceil(roundedSpeed) * 10 : roundedSpeed;
 
         if (transportSpeed<40 & city.isOnWater()){
-            return new Ship(shipName, transportWeight, transportSpeed, shipCostOfKm);
+            return new Ship(SHIP_NAME, transportWeight, transportSpeed, SHIP_COST_OF_KM);
         }
         if (transportSpeed>120 & city.hasAirport()){
-            return new Plane(planeName, transportWeight, transportSpeed, planeCostOfKm);
+            return new Plane(PLANE_NAME, transportWeight, transportSpeed, PLANE_COST_OF_KM);
         }
-        return new Truck( truckName, transportWeight, transportSpeed, truckCostOfKm);
+        return new Truck(TRUCK_NAME, transportWeight, transportSpeed, TRUCK_COST_OF_KM);
     }
 }
 
